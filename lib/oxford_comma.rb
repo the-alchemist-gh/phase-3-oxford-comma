6d79210b44f3564cc702695af2141d7dcb33963d
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-08-22 12:10:26 +0000

    Completed lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+# require "pry"
 def oxford_comma(array)
 
+  if(array.length < 2)
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else 
+    array[-1] = "and #{array.last}"
+    array.join(", ")
+  end
+  
 end
+
+# oxford_comma(["fiddleheads", "okra", "kohlrabi", "dragon fruits"])
+# binding.pry
